bd24bed7e312cbd8b0979d688f943dc3f3a57c97
Eduardo Martins <emartinslemos@gmail.com>
2022-12-05 21:21:56 -0400

    menu + organização de repositorio

---
+++ app/src/main/java/com/quanz/qualisapp/repository/QualisRepository.kt
@@ -0,0 +1,90 @@
+package com.quanz.qualisapp.repository
+
+import android.content.Context
+import androidx.room.Room
+import com.quanz.qualisapp.db.QualisDatabase
+import com.quanz.qualisapp.db.entities.ConferenciaEntity
+import com.quanz.qualisapp.db.entities.CorrelacaoComOutraAreaEntity
+import com.quanz.qualisapp.db.entities.PeriodicoEntity
+import com.quanz.qualisapp.service.QualisAppService
+import com.quanz.qualisapp.ui.MainActivity
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class QualisRepository(
+    private val context: Context
+) {
+
+    private val service: QualisAppService = montaRetrofit().create(QualisAppService::class.java)
+
+    private val qualisDatabase by lazy {
+        Room.databaseBuilder(
+            context, QualisDatabase::class.java, MainActivity.DATABASE_NAME
+        ).build()
+    }
+
+    suspend fun getPeriodico() {
+        val periodicos = service.getPeriodicos()
+        savePeriodico(periodicos.periodico)
+    }
+
+    suspend fun getConferencia() {
+        val conferencias = service.getConferencias()
+        saveConferencia(conferencias.conferencia)
+
+    }
+
+    suspend fun getCorrelacoes() {
+            val correlacoes = service.getCorrelacaoOutrasAreas()
+            saveCorrelacoes(correlacoes.correlacaoComOutrasAreas)
+    }
+
+    private suspend fun saveCorrelacoes(correlacoes: List<List<String>>) {
+        for (correlacao in correlacoes) {
+            qualisTable().insertCorrelacaoComOutraArea(
+                CorrelacaoComOutraAreaEntity(
+                    issn = correlacao[0],
+                    periodico = correlacao[1],
+                    extratoCapesComp = correlacao[2],
+                    extratoCapes = correlacao[3],
+                    area = correlacao[4]
+                )
+            )
+        }
+    }
+
+    private suspend fun saveConferencia(conferencias: List<List<String>>) {
+        for (conferencia in conferencias) {
+            qualisTable().insertConferencia(
+                ConferenciaEntity(
+                    siglas = conferencia[0],
+                    nome = conferencia[1],
+                    extratoCapes = conferencia[2]
+                )
+            )
+        }
+    }
+
+    private suspend fun savePeriodico(periodicos: List<List<String>>) {
+        for (periodico in periodicos) {
+            qualisTable().insertPeriodico(
+                PeriodicoEntity(
+                    issn = periodico[0],
+                    nome = periodico[1],
+                    extratoCapes = periodico[2]
+                )
+            )
+        }
+    }
+
+    private fun montaRetrofit(): Retrofit {
+        return Retrofit
+            .Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(MainActivity.BASE_URL)
+            .build()
+    }
+
+    private fun qualisTable() = qualisDatabase.qualisDao()
+
+}
--- app/src/main/java/com/quanz/qualisapp/ui/MainActivity.kt
+++ app/src/main/java/com/quanz/qualisapp/ui/MainActivity.kt
@@ -1,108 +1,53 @@
 package com.quanz.qualisapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.coroutineScope
-import androidx.room.Room.databaseBuilder
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.quanz.qualisapp.R
-import com.quanz.qualisapp.db.QualisDatabase
-import com.quanz.qualisapp.db.entities.ConferenciaEntity
-import com.quanz.qualisapp.db.entities.CorrelacaoComOutraAreaEntity
-import com.quanz.qualisapp.db.entities.PeriodicoEntity
-import com.quanz.qualisapp.service.models.PeriodicosData
+import com.quanz.qualisapp.repository.QualisRepository
 import com.quanz.qualisapp.service.QualisAppService
 import kotlinx.coroutines.launch
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private val repository: QualisRepository = QualisRepository(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        getPeriodico()
+        getAllinformacoes()
     }
 
-    private val service: QualisAppService = montaRetrofit().create(QualisAppService::class.java)
-
-    private lateinit var periodico: PeriodicosData
-    private val qualisDatabase by lazy {
-        databaseBuilder(
-            this, QualisDatabase::class.java, DATABASE_NAME
-        ).build()
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_qualis, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 
-    private fun montaRetrofit(): Retrofit {
-        return Retrofit
-            .Builder()
-            .addConverterFactory(GsonConverterFactory.create())
-            .baseUrl(BASE_URL)
-            .build()
-    }
-
-    fun getPeriodico() {
-        lifecycle.coroutineScope.launch {
-            val periodicos = service.getPeriodicos()
-            savePeriodico(periodicos.periodico)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.botao_atualizar -> {
+                getAllinformacoes()
+                true
+            }
+            else -> false
         }
     }
 
-    fun getConferencia() {
-        lifecycle.coroutineScope.launch {
-            val conferencias = service.getConferencias()
-            saveConferencia(conferencias.conferencia)
+    private fun getAllinformacoes() {
+        lifecycleScope.launch {
+            repository.getPeriodico()
+            repository.getConferencia()
+            repository.getCorrelacoes()
         }
     }
-
-    fun getCorrelacoes(){
-        lifecycle.coroutineScope.launch {
-            val correlacoes = service.getCorrelacaoOutrasAreas()
-            saveCorrelacoes(correlacoes.correlacaoComOutrasAreas)
-        }
-    }
-
-    private suspend fun saveCorrelacoes(correlacoes: List<List<String>>) {
-        for (correlacao in correlacoes){
-            qualisTable().insertCorrelacaoComOutraArea(
-                CorrelacaoComOutraAreaEntity(
-                    issn = correlacao[0],
-                    periodico = correlacao[1],
-                    extratoCapesComp = correlacao[2],
-                    extratoCapes = correlacao[3],
-                    area = correlacao[4]
-                )
-            )
-        }
-    }
-
-    private suspend fun saveConferencia(conferencias: List<List<String>>) {
-        for (conferencia in conferencias) {
-            qualisTable().insertConferencia(
-                ConferenciaEntity(
-                    siglas = conferencia[0],
-                    nome = conferencia[1],
-                    extratoCapes = conferencia[2]
-                )
-            )
-        }
-    }
-
-    private suspend fun savePeriodico(periodicos: List<List<String>>) {
-        for (periodico in periodicos) {
-            qualisTable().insertPeriodico(
-                PeriodicoEntity(
-                    issn = periodico[0],
-                    nome = periodico[1],
-                    extratoCapes = periodico[2]
-                )
-            )
-        }
-    }
-
-    private fun qualisTable() = qualisDatabase.qualisDao()
 
     companion object {
-        private const val BASE_URL = "https://qualis.ic.ufmt.br/"
-        private const val DATABASE_NAME = "DATABASE_NAME"
+        const val BASE_URL = "https://qualis.ic.ufmt.br/"
+        const val DATABASE_NAME = "DATABASE_NAME"
     }
 
 }
